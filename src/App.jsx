1962adbda504f24ee1fc2d3c1212b4e4dc470187
praneeth00007 <2300031137@kluniversity.in>
2025-07-30 10:49:44 +0530

    Modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className='container'>
       <h1>Welcome to my React Project - S105</h1>
+      <h1>CI/CD and Cloud Devops Course</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite</p>
     </div>
   )
