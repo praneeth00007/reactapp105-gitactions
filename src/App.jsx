a7f27dcb287aea2c4cf123bc4e36b4c3a138e0c3
praneeth00007 <2300031137@kluniversity.in>
2025-07-30 10:17:18 +0530

    React project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <h1>Welcome to my React Project - S105</h1>
+      <p>This is a simple homepage deployed on GitHub Pages using Vite</p>
+    </div>
+  )
+}
+
