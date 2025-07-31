753927a5e909527f955fc61ea34698fc642b9eb0
praneeth00007 <2300031137@kluniversity.in>
2025-07-31 12:36:11 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
       <h1>Welcome to my React Project - S105</h1>
       <h1>CI/CD and Cloud Devops Course</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite</p>
+      <p>Done with git pages/actions </p>
     </div>
   )
 }
